06f057af2dd201e6855e792cbc48bb7d75f2dc0e
phanuphong <phanuphong.bo@ku.th>
2026-09-18 11:51:59 +0700

    week09

---
+++ labs/week09-lab/example/try.py
@@ -0,0 +1,16 @@
+'''
+try:
+    score= float(input("กรอกคะแนน"))
+
+
+except ValueError as error:
+        ZeroDivisionError
+        RuntimeError
+        Exception
+    print(f"ข้อมูลไม่ถูกต้อง{error}")
+
+except ValueError
+except ZeroDivisionError
+except RuntimeError
+except Exception
+'''
