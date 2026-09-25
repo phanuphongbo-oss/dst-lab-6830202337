d886a091a81c91436dbfb95796ce2cfe2a04d391
Phanuphong bunnan <phanuphong.bo@ku.th>
2026-09-25 11:54:40 +0700

    rainy day

--- assignments/week07/quiz-1.py
+++ assignments/week07/quiz-1.py
@@ -12,13 +12,32 @@ class Rectangle:
 
     # Method to get the area
     def get_area(self):
-        pass
+        return self.length * self.width
 
     # Method to get the perimeter
     def get_perimeter(self):
-        pass
+        return 2 * (self.length * self.width)
 
 
 rect = Rectangle(10, 5)
+print(rect.get_area())       # Should print 50
+print(rect.get_perimeter())  # Should print 30
+
+
+class Circle:
+    def __init__(self,radius):
+        self.radius = radius
+        
+
+    # Method to get the area
+    def get_area(self):
+        return  3.14 * (self.radius **2)
+
+    # Method to get the perimeter
+    def get_perimeter(self):
+        return 2 * 3.14 * self.radius
+
+
+rect = Circle(10)
 print(rect.get_area())       # Should print 50
 print(rect.get_perimeter())  # Should print 30
